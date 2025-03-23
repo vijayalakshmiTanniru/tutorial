9df355b888dd1ec8d8e921eae7365eb521f0bcda
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 20:30:56 +0530

    Update Node.js

--- Backend/Node.js
+++ Backend/Node.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+const URI = "YOUR_MONGODB_ATLAS_URI"; // Replace with your URI
 
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("Could not connect to MongoDB:", err));
