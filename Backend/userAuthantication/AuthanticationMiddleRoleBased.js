e4959d5bbf4e35ee0c699d12ff93df281fd165f2
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 21:02:49 +0530

    Delete Backend directory/Node/index.js

--- Backend/userAuthantication/AuthanticationMiddleRoleBased.js
+++
@@ -1,9 +0,0 @@
-
-exports.authorize = (roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            return res.status(403).json({ message: "Unauthorized" });
-        }
-        next();
-    };
-};
--- Backend/userAuthantication/Logout.js
+++
@@ -1,4 +0,0 @@
-exports.logout = (req, res) => {
-    res.cookie('jwt', '', { httpOnly: true, maxAge: 0 }); // Clear the cookie
-    res.json({ message: "Logged out successfully" }); //access to protected routes removed upon logout [cite: 3]
-};
