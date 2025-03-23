82501b52bb84e02a8ffc597c74428c8670342aeb
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 20:46:11 +0530

    Update Logout.js

--- Backend/userAuthantication/Logout.js
+++ Backend/userAuthantication/Logout.js
@@ -1,1 +1,4 @@
-
+exports.logout = (req, res) => {
+    res.cookie('jwt', '', { httpOnly: true, maxAge: 0 }); // Clear the cookie
+    res.json({ message: "Logged out successfully" }); //access to protected routes removed upon logout [cite: 3]
+};
