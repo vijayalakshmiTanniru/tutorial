ab19d243e4c42726b96010cce83c3a51a53007cc
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 21:20:48 +0530

    Update Signup.js

--- Backend/UserAuthentication/Signup.js
+++ Backend/UserAuthentication/Signup.js
@@ -1,1 +1,16 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
+exports.signup = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
