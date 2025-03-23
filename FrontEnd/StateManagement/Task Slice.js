1e0ca79c564adabfc4138433f7d0981fb1bd8588
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 21:39:27 +0530

    Update Task Slice.js

--- FrontEnd/StateManagement/Task Slice.js
+++ FrontEnd/StateManagement/Task Slice.js
@@ -1,1 +1,14 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+    tasks:,
+    loading: false,
+    error: null,
+};
+
+export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+    const response = await axios.get('/api/tasks');
+    return response.data;
+});
 
