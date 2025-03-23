609888077d295185e7746448026efb1f4fb5d889
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 21:36:41 +0530

    Update storeSetup.js

--- FrontEnd/StateManagement/storeSetup.js
+++ FrontEnd/StateManagement/storeSetup.js
@@ -1,1 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../slices/authSlice'; // You'll create this
+import taskReducer from '../slices/taskSlice'; // You'll create this
 
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        task: taskReducer,
+    },
+});
