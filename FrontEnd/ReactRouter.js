c7c379a8900b746ac883ccc1ac0930d544ac4071
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 21:35:15 +0530

    Update ReactRouter.js

--- FrontEnd/ReactRouter.js
+++ FrontEnd/ReactRouter.js
@@ -1,1 +1,20 @@
+import React from 'react';
+  import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+  import Signup from './components/Signup';
+  import Login from './components/Login';
+  import TaskList from './components/TaskList';
+  import PrivateRoute from './components/PrivateRoute'; // You'll create this
 
+  function App() {
+      return (
+          <Router>
+              <Switch>
+                  <Route path="/signup" component={Signup} />
+                  <Route path="/login" component={Login} />
+                  <PrivateRoute path="/" component={TaskList} />
+              </Switch>
+          </Router>
+      );
+  }
+
+  export default App;
