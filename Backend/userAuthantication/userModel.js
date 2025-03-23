a4deb283dfa7c02b4e787648363f7cec98b275b9
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-03-23 20:35:05 +0530

    Update userModel.js

--- Backend/userAuthantication/userModel.js
+++ Backend/userAuthantication/userModel.js
@@ -1,4 +1,4 @@
-onst mongoose = require('mongoose');
+Const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
